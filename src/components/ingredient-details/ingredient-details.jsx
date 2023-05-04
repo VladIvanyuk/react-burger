8d0a1b2cac5b249f36f8e3b3856e7e24ed5ae6ff
Vlad <ivanyuk_vv@vld-webdev.com>
2023-05-04 15:31:47 +1000

    сделал вывод описания онгридиента

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,26 +1,29 @@
 import styles from './ingredient-details.module.css';
 
-export const IngredientDetails = (props) => {
+export const IngredientDetails = ({ ingredient }) => {
+
+  const { name, image_large, calories, fat, carbohydrates, proteins } = ingredient;
+
   return (
     <div className={styles.ingredientBlock}>
-      <img src="https://code.s3.yandex.net/react/code/bun-02-large.png" alt="Краторная булка N-200i" className='mb-4' />
-      <p className="text text_type_main-medium mb-8">Краторная булка N-200i</p>
+      <img src={image_large} alt={name} className='mb-4' />
+      <p className="text text_type_main-medium mb-8">{name}</p>
       <ul className={`${styles.list} mb-15`}>
         <li className={`${styles.item} text text_type_main-default mr-5`}>
           <p>Калории, ккал</p>
-          <p>2674</p>
+          <p>{calories}</p>
         </li>
         <li className={`${styles.item} text text_type_main-default mr-5`}>
           <p>Белки, г</p>
-          <p>80</p>
+          <p>{proteins}</p>
         </li>
         <li className={`${styles.item} text text_type_main-default mr-5`}>
           <p>Жиры, г</p>
-          <p>800</p>
+          <p>{fat}</p>
         </li>
         <li className={`${styles.item} text text_type_main-default`}>
           <p>Углеводы, г</p>
-          <p>300</p>
+          <p>{carbohydrates}</p>
         </li>
       </ul>
     </div>
