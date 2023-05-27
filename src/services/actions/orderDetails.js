ac847d9719f82a998bc5da47265a04a8f7d60c66
Vladislav Ivanyuk <vl.ivanyuk1994@gmail.com>
2023-05-27 13:31:39 +1000

    [~] переделал структуру запросов

--- src/services/actions/orderDetails.js
+++ src/services/actions/orderDetails.js
@@ -1,11 +1,32 @@
-import { postData } from "../../utils/burger-api";
-export const GET_ORDER_DETAILS = 'GET_ORDER_DETAILS';
+import { request } from "../../utils/burger-api";
+export const GET_ORDER_DETAILS = "GET_ORDER_DETAILS";
+export const GET_ORDER_DETAILS_SUCCESS = "GET_ORDER_DETAILS_SUCCESS";
+export const GET_ORDER_DETAILS_FAILED = "GET_ORDER_DETAILS_FAILED";
 
 export const getOrderDetails = (idList) => {
-    return function(dispatch) {
-        postData(idList).then((data) => dispatch({
-            type: GET_ORDER_DETAILS,
-            details: data
-        }))
-    }
-}
+  return function (dispatch) {
+    dispatch({
+        type: GET_ORDER_DETAILS
+    })
+
+    request("orders", {
+      // Метод, если не указывать, будет использоваться GET
+      method: "POST",
+      // Заголовок запроса
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Данные
+      body: JSON.stringify(idList),
+    }).then((data) =>
+      dispatch({
+        type: GET_ORDER_DETAILS_SUCCESS,
+        details: data,
+      })
+    ).catch((err) => {
+        dispatch({
+            type: GET_ORDER_DETAILS_FAILED
+        })
+    })
+  };
+};
--- src/services/reducers/orderDetails.js
+++ src/services/reducers/orderDetails.js
@@ -1,10 +1,29 @@
-import { GET_ORDER_DETAILS } from '../actions/orderDetails';
+import { GET_ORDER_DETAILS, GET_ORDER_DETAILS_FAILED, GET_ORDER_DETAILS_SUCCESS } from '../actions/orderDetails';
+const initialState = {
+    details: {},
+    isOrderError: false,
+    isOrderLoaded: false
+}
 
-export const orderDetails = (state = {}, action) => {
+export const orderDetails = (state = initialState, action) => {
     switch(action.type) {
         case GET_ORDER_DETAILS:
             return {
-                ...action.details
+                ...state,
+                isOrderLoaded: false,
+                isOrderError: false
+            }
+        case GET_ORDER_DETAILS_SUCCESS:
+            return {
+                ...state,
+                details: action.details,
+                isOrderLoaded: true
+            }
+
+        case GET_ORDER_DETAILS_FAILED:
+            return {
+                ...state,
+                isOrderError: true
             }
         default: 
             return state;
