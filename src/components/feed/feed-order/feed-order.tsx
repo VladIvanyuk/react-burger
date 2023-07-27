361f2a940ee2d86801894e3565cfbc9246418a6c
VladIvanyuk <vl.ivanyuk1994@gmail.com>
2023-07-27 14:28:36 +1000

    [+] сделал вёрстку заказа в общей ленте

--- src/components/feed/feed-order/feed-order.tsx
+++ src/components/feed/feed-order/feed-order.tsx
@@ -9,7 +9,15 @@ export const FeedOrder: React.FC = (props) => {
     "https://code.s3.yandex.net/react/code/sauce-01-mobile.png",
     "https://code.s3.yandex.net/react/code/sauce-02-mobile.png",
     "https://code.s3.yandex.net/react/code/meat-02-mobile.png",
+    "https://code.s3.yandex.net/react/code/meat-02-mobile.png",
+    "https://code.s3.yandex.net/react/code/meat-02-mobile.png",
+    "https://code.s3.yandex.net/react/code/meat-02-mobile.png",
   ];
+  // если ингридиентов больше 6, откусываем от основного массива для вывода
+  const imgElemsForRender =
+    imgMockList.length > 6 ? [...imgMockList].splice(0, 6) : imgMockList;
+  const imgListDiff = imgMockList.length - 6;
+
   return (
     <div className={`${styles.order} p-6`}>
       <div className={`${styles.orderHeader} mb-6`}>
@@ -25,16 +33,43 @@ export const FeedOrder: React.FC = (props) => {
       </div>
       <div className={styles.orderFooter}>
         <div>
-          {imgMockList.map((img, index) => {
-
+          {imgElemsForRender.map((img, index) => {
+            // если ингредиентов больше 6, то рисуем картинку ингредиента с разницей
             return (
-            <div className={styles.orderImageWrapper} style={{
-              'left': 35*index + 'px',
-              'zIndex': (imgMockList.length * 10) - (index * 10),
-            }}>
-              <img key={index} className={styles.orderImage} src={img} alt="Картинка продукта" />
-            </div>
-            )
+              <div
+                key={index}
+                className={styles.orderImageWrapper}
+                style={{
+                  left: 40 * index + "px",
+                  zIndex: imgElemsForRender.length * 10 - index * 10,
+                  backgroundColor:
+                    index === 5 && imgListDiff > 0
+                      ? "rgba(0, 120, 201, 0.7)"
+                      : "none",
+                }}
+              >
+                {index === 5 && imgListDiff > 0 ? (
+                  <>
+                    <span className={styles.imgDiffCounter}>
+                      <span className="text text_type_main-default">
+                        +{imgListDiff}
+                      </span>
+                    </span>
+                    <img
+                      className={styles.orderImage}
+                      src={img}
+                      alt="Картинка продукта"
+                    />
+                  </>
+                ) : (
+                  <img
+                    className={styles.orderImage}
+                    src={img}
+                    alt="Картинка продукта"
+                  />
+                )}
+              </div>
+            );
           })}
         </div>
         <div className={styles.orderPrice}>
